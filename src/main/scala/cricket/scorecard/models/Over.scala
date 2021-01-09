3893ea75c0bafad35b5f92ddf2e2bb29c26a8a8f
Swapnil Khandekar <swapnikh@thoughtworks.com>
2021-01-09 20:46:20 +0530

    Enable over to calculate valid balls and if over is finished

--- src/main/scala/cricket/scorecard/models/Over.scala
+++ src/main/scala/cricket/scorecard/models/Over.scala
@@ -1,6 +1,10 @@
 package cricket.scorecard.models
 
-case class Over(number: Int, balls: List[Ball])
+case class Over(number: Int, balls: List[Ball]) {
+  def validCount: Int = balls.count(b => b != WideBall && b != NoBall)
+
+  def isFinished: Boolean = validCount == 6
+}
 
 object Over {
   def empty(number: Int): Over = Over(number, List.empty)
---
+++ src/test/scala/cricket/scorecard/models/OverTest.scala
@@ -0,0 +1,28 @@
+package cricket.scorecard.models
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class OverTest extends AnyWordSpec with Matchers {
+  "validCount" should {
+    "return count of balls except wide and no balls" in {
+      val over = Over(1, List(Runs(1), Runs(2), WideBall, NoBall))
+
+      over.validCount shouldBe 2
+    }
+  }
+
+  "isFinished" should {
+    "return true when valid balls are 6" in {
+      val over = Over(1, List(Runs(1), Runs(2), Runs(6), NoBall, Runs(4), Runs(4), Runs(1)))
+
+      over.isFinished shouldBe true
+    }
+
+    "return false when valid balls are less than 6" in {
+      val over = Over(1, List(Runs(1), Runs(2)))
+
+      over.isFinished shouldBe false
+    }
+  }
+}
