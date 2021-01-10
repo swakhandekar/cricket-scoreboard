fc0b929b656f9cd0501eb35979eb8754fd2288d3
Swapnil Khandekar <swapnikh@thoughtworks.com>
2021-01-10 11:27:29 +0530

    Fix bug to calculate valid balls in an over

--- src/main/scala/cricket/scorecard/models/Over.scala
+++ src/main/scala/cricket/scorecard/models/Over.scala
@@ -1,7 +1,11 @@
 package cricket.scorecard.models
 
 case class Over(number: Int, balls: List[Ball]) {
-  def validCount: Int = balls.count(b => b != WideBall && b != NoBall)
+  def validCount: Int = balls.count({
+    case _: Runs => true
+    case Wicket => true
+    case _ => false
+  })
 
   def isFinished: Boolean = validCount == 6
 }
