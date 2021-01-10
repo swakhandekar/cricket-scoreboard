1a17c9c77ebe4f34083897a87969f697a71be60e
Swapnil Khandekar <swapnikh@thoughtworks.com>
2021-01-10 22:22:46 +0530

    Mark end of match when team 2 makes more run than team one

--- src/main/scala/cricket/scorecard/Match.scala
+++ src/main/scala/cricket/scorecard/Match.scala
@@ -3,11 +3,14 @@ package cricket.scorecard
 import cricket.scorecard.models.Ball
 
 case class Match(inning1: Inning, inning2: Inning, isSecondInning: Boolean = false, hasEnded: Boolean = false) {
+
+
   def nextBall(ball: Ball): Match = {
     if(isSecondInning) {
       val updatedInning = inning2.nextBall(ball)
+      val teamMadeMoreScore = updatedInning.scoreCard.totalScore > inning1.scoreCard.totalScore
 
-      if(updatedInning.endOfInning) copy(inning2 = updatedInning, hasEnded = true)
+      if(updatedInning.endOfInning || teamMadeMoreScore) copy(inning2 = updatedInning, hasEnded = true)
       else copy(inning2 = updatedInning)
     } else {
       val updatedInning = inning1.nextBall(ball)
--- src/test/scala/cricket/scorecard/MatchTest.scala
+++ src/test/scala/cricket/scorecard/MatchTest.scala
@@ -1,6 +1,6 @@
 package cricket.scorecard
 
-import cricket.scorecard.models.Runs
+import cricket.scorecard.models.{Player, Runs}
 import org.mockito.MockitoSugar.{mock, when}
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
@@ -43,6 +43,10 @@ class MatchTest extends AnyWordSpec with Matchers {
         val mockInning1 = mock[Inning]
         val mockInning2 = mock[Inning]
         val updatedInning = mock[Inning]
+        val scoreCard1 = ScoreCard(8, mock[Player], mock[Player])
+        val scoreCard2 = ScoreCard(10, mock[Player], mock[Player])
+        when(updatedInning.scoreCard).thenReturn(scoreCard2)
+        when(mockInning1.scoreCard).thenReturn(scoreCard1)
         when(mockInning2.nextBall(nextBall)).thenReturn(updatedInning)
         val cricketMatch = Match(mockInning1, mockInning2, isSecondInning = true)
 
@@ -58,6 +62,29 @@ class MatchTest extends AnyWordSpec with Matchers {
         val updatedInning = mock[Inning]
         when(updatedInning.endOfInning).thenReturn(true)
         when(mockInning2.nextBall(nextBall)).thenReturn(updatedInning)
+        val scoreCard1 = ScoreCard(8, mock[Player], mock[Player])
+        val scoreCard2 = ScoreCard(5, mock[Player], mock[Player])
+        when(updatedInning.scoreCard).thenReturn(scoreCard2)
+        when(mockInning1.scoreCard).thenReturn(scoreCard1)
+
+        val cricketMatch = Match(mockInning1, mockInning2, isSecondInning = true)
+
+        val updatedMatch = cricketMatch.nextBall(nextBall)
+
+        updatedMatch.hasEnded shouldBe true
+      }
+
+      "mark match finished when team makes more runs that team 1" in {
+        val nextBall = Runs(1)
+        val mockInning1 = mock[Inning]
+        val mockInning2 = mock[Inning]
+        val updatedInning = mock[Inning]
+        val scoreCard1 = ScoreCard(8, mock[Player], mock[Player])
+        val scoreCard2 = ScoreCard(10, mock[Player], mock[Player])
+        when(updatedInning.endOfInning).thenReturn(false)
+        when(updatedInning.scoreCard).thenReturn(scoreCard2)
+        when(mockInning2.nextBall(nextBall)).thenReturn(updatedInning)
+        when(mockInning1.scoreCard).thenReturn(scoreCard1)
         val cricketMatch = Match(mockInning1, mockInning2, isSecondInning = true)
 
         val updatedMatch = cricketMatch.nextBall(nextBall)
