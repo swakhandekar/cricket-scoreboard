aa4633e8fe48b0710004f05b0ce4b43e1e565c6c
Swapnil Khandekar <swapnikh@thoughtworks.com>
2021-01-11 18:32:34 +0530

    Fix sample Input2 to return players as list of string

--- src/main/scala/cricket/scorecard/Input2.scala
+++ src/main/scala/cricket/scorecard/Input2.scala
@@ -4,8 +4,8 @@ import cricket.scorecard.models._
 
 object Input2 {
   val numOvers = 2
-  val team1Players: List[Player] = List("P1", "P2", "P3", "P4", "P5").map(Player(_))
-  val team2Players: List[Player] = List("P6", "P7", "P8", "P9", "P10").map(Player(_))
+  val team1Players: List[String] = List("P1", "P2", "P3", "P4", "P5")
+  val team2Players: List[String] = List("P6", "P7", "P8", "P9", "P10")
 
   val balls: List[Ball] = List(
     // inning 1
